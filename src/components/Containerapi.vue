b99b4f7c6882476105b293b3b27b1a9721cd4682
Jose <Jose>
2019-02-27 15:12:11 -0300

    the button of pokemon's information is ready

--- src/components/Containerapi.vue
+++ src/components/Containerapi.vue
@@ -18,15 +18,13 @@ export default {
 
   data () {
     return {
-      pokemonReceived: null,
-      pokelink: null
-    }
+      pokemonReceived: null
+     }
   },
 
   methods: {
     getPokemon (search) {
       axios.get('https://pokeapi.co/api/v2/pokemon/' + search).then(response => (this.pokemonReceived = response))
-      axios.get('https://www.google.com/search?ei=16Z2XJ2_Nfiu5OUPvdqksAQ&q=pikachu&oq=pikachu&gs_l=psy-ab.3..0i67j0j0i131i67l2j0i131l2j0i67j0l3.371419.372711..373087...1.0..0.217.1274.0j5j2......0....1..gws-wiz.....6..0i71j35i39.TQvD99CaPA4').then(response => (this.pokelink = response))
     }
   },
 
--- src/components/pokemon.vue
+++ src/components/pokemon.vue
@@ -6,7 +6,7 @@
     <ul class="list-abilities">
       <li :key="item.index" v-for="item in pokemon.data.abilities">{{item.ability.name}}</li>
     </ul>
-    <a style="color: white;">click here for more informations about this pokemon</a>
+    <a :href="pokelink" target="_blank">click here for more informations about this pokemon</a>
   </div>
 </template>
 
@@ -15,11 +15,12 @@
 export default {
   data () {
     return {
-
+      pokelink: null
     }
   },
 
   updated () {
+    this.pokelink = 'https://www.google.com/search?q=' + this.pokemon.data.name
     console.log(this.pokemon)
   },
 
